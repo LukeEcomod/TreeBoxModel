1ad9ff31018410782b39306421746f876dd5467b
Altesmi <optikkanen@gmail.com>
2020-03-24 17:29:34 +0200

    iotools tests added

--- src/test/test_iotools.py
+++ src/test/test_iotools.py
@@ -4,24 +4,45 @@ from typing import List
 from ..tools.iotools import initialize_netcdf, write_netcdf
 import os
 import pytest
+import xarray as xr
+import numpy as np
 
 
 @pytest.fixture(scope="function")
-def iomodel(test_model):
+def ncf(test_model):
 
     test_model.outputfile = "iotools_test.nc"
-    return test_model
-
-
-def test_ncf_inif(iomodel):
     variables = {'radius': ['radius of an element', 'm', ("index", "radial_layers", "axial_layers"), 'f4'],
-                 'pressures': ['pressure of an element', 'Pa', ("index", "radial_layers", "axial_layers"), 'f4'],
+                 'pressure': ['pressure of an element', 'Pa', ("index", "radial_layers", "axial_layers"), 'f4'],
                  'elastic modulus':
                  ['elastic modulus of an element', 'Pa', ("index", "radial_layers", "axial_layers"), 'f4'],
                  'num_elements':
-                 ['number of tree elements', 'pcs.', ("index", "radial_layers", "axial_layers"), 'i4'],
-                 'simTime': ['Time in simulation', 's', ("index", "radial_layers", "axial_layers"), 'f4']}
-    ncf = initialize_netcdf(iomodel, variables)
-    assert ncf.variables['simTime'].units == 's'
+                 ['number of tree elements', 'pcs.', ("index"), 'i4'],
+                 'simTime': ['Time in simulation', 's', ("index"), 'f4']}
+
+    ncf = initialize_netcdf(test_model, variables)
+    yield ncf
+    print('\n Cleaning up NetCDF4 file')
     ncf.close()
-    # os.remove(iomodel.outputfile)
+    os.remove(test_model.outputfile)
+    print('\n Cleanup finished')
+
+
+def test_ncf_inif(ncf):
+    assert ncf.variables['simTime'].units == 's'
+
+
+def test_ncf_writer(ncf):
+    # add random data to pressures and simTime
+    random_pressure_data = np.random.uniform(size=(2, 40, 2))
+    random_simTime_data = np.random.uniform(size=(2, 1))
+    results = {'simTime': random_simTime_data[0], 'pressure': random_pressure_data[0, :, :].reshape(40, 2)}
+    write_netcdf(ncf, results)
+    # write again
+    results = {'simTime': random_simTime_data[1], 'pressure': random_pressure_data[1, :, :].reshape(40, 2)}
+    write_netcdf(ncf, results)
+    # read the netcdf file with xarray
+    data = xr.open_dataset('iotools_test.nc')
+    # check that the sums match
+    assert float(np.sum(data['pressure'])) == pytest.approx(np.sum(random_pressure_data), rel=1e-6)
+    assert float(np.sum(data['simTime'])) == pytest.approx(np.sum(random_simTime_data), rel=1e-6)
--- src/tools/iotools.py
+++ src/tools/iotools.py
@@ -1,5 +1,5 @@
-from typing import Dict
-from netCDF4 import Dataset
+from typing import Dict, List
+from netCDF4 import Dataset, Variable
 from ..model import Model
 from ..constants import MAX_ELEMENT_COLUMNS
 import os.path
@@ -18,22 +18,26 @@ def initialize_netcdf(model: Model, variables: Dict) -> Dataset:
     ncf.createDimension("radial_layers", MAX_ELEMENT_COLUMNS)
     ncf.createDimension("axial_layers", model.tree.num_elements)
 
-    # create variables
+    # create the index variable
+    ind: Variable = ncf.createVariable('index', 'i4', ('index'))
+    ind.description = 'NETCDF index variable'
+    # create the other variables
     for var_name in variables.keys():
         description: str = variables[var_name][0]
         units: str = variables[var_name][1]
         dimensions: str = variables[var_name][2]
         datatype: str = variables[var_name][3]
-        new_var = ncf.createVariable(var_name, datatype, dimensions)
+        new_var: Variable = ncf.createVariable(var_name, datatype, dimensions)
         new_var.units = units
         new_var.description = description
 
     return ncf
 
 
-def write_netcdf(ncf: Dataset, results: Dict):
-    ind: int = ncf['index'].shape[0] - 1
+def write_netcdf(ncf: Dataset, results: Dict) -> None:
+    ind: int = ncf['index'].shape[0]
     index_dim_vars: List = ['num_elements', 'simTime', 'index']
+    ncf['index'][ind] = ind
     for key in results.keys():
         if key in index_dim_vars:
             # variable has dimension ("index")
